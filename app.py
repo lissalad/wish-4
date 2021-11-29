c2b00ac97cf8de6ca73d9b5aa56ebb3416669ba3
lissalad <elissalaymon@gmail.com>
2021-11-29 12:03:47 -0800

    basic routing, creating and viewing lists, done, needs items

--- app.py
+++ app.py
@@ -7,15 +7,39 @@ import certifi
 host = os.environ.get("DB_URL")
 client = MongoClient(host, tlsCAFile=certifi.where())
 db = client.wish4
-playlists = db.wishlists
-comments = db.items
+lists = db.lists
+items = db.items
 
 app = Flask(__name__)
 
 # ----------- PAGES ----------------------------------------- #
 @app.route('/')
-def playlists_index():
+def wish4_index():
     return render_template('index.html')
+
+@app.route('/wishlists')
+def wishlists():
+    return render_template('wishlists.html', lists=lists.find())
+
+@app.route('/wishlists/new')
+def wishlists_new():
+    return render_template('wishlists_new.html')
+
+@app.route('/wishlists/<wishlist_id>')
+def wishlists_show(wishlist_id):
+  wishlist = lists.find_one({'_id':ObjectId(wishlist_id)})
+  return render_template('wishlists_view.html', wishlist=wishlist)
+
+
+# ----------- CREATE WISHLIST --------------------------------- #
+@app.route('/wishlists', methods=['POST'])
+def wishlist_create():
+  wishlist = {
+    'title': request.form.get('title'),
+    'description': request.form.get('description')
+    }
+  lists.insert_one(wishlist)
+  return redirect(url_for('wish4_index'))
 
 
 # ----------- RUN ----------------------------------------------------- #
