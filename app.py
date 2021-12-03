4107c65e1890e894d9ddb9f832ac4bb936f5b489
lissalad <elissalaymon@gmail.com>
2021-12-03 00:07:28 -0800

    edit update delete for both lists and items :)

--- app.py
+++ app.py
@@ -36,7 +36,19 @@ def wishlists_show(wishlist_id):
 def add_item(wishlist_id):
   return render_template('add_item.html', wishlist_id=wishlist_id)
 
-# ----------- CREATE WISHLIST --------------------------------- #
+@app.route('/wishlists/<wishlist_id>/edit')
+def edit_wishlist(wishlist_id):
+    wishlist = lists.find_one({'_id':ObjectId(wishlist_id)})
+    return render_template('wishlist_edit.html', wishlist=wishlist)
+
+@app.route('/wishlists/<item_id>/edit-item')
+def edit_item(item_id):
+    item = items.find_one({'_id':ObjectId(item_id)})
+    wishlist = lists.find_one({'_id':item['wishlist_id']})
+    return render_template('item_edit.html', item=item,wishlist=wishlist)
+
+
+# ----------- WISHLIST --------------------------------- #
 @app.route('/wishlists', methods=['POST'])
 def wishlist_create():
   wishlist = {
@@ -44,9 +56,25 @@ def wishlist_create():
     'description': request.form.get('description')
     }
   lists.insert_one(wishlist)
-  return redirect(url_for('wishlists'))#('wishlists_show'), wishlist_id = wishlist['_id']) # GO TO NEW WISHLIST HOW?
+  # test=  lists.insert_one(wishlist)
+  # print(test)
+  return redirect(url_for('wishlists_show', wishlist_id=wishlist['_id']))
 
-# ----------- CREATE ITEM --------------------------------- #
+@app.route('/wishlists/<wishlist_id>/delete')
+def wishlist_delete(wishlist_id):
+  lists.delete_one({'_id': ObjectId(wishlist_id)})  
+  return redirect(url_for('wishlists'))
+
+@app.route('/wishlists/<wishlist_id>', methods=['POST'])
+def wishlist_update(wishlist_id):
+  updated = {
+  'title': request.form.get('title'),
+  'description': request.form.get('description')
+  }
+  lists.update_one({'_id':ObjectId(wishlist_id)}, {'$set': updated})
+  return redirect(url_for('wishlists_show', wishlist_id=wishlist_id))
+
+# ----------- ITEM --------------------------------- #
 @app.route('/wishlists/<wishlist_id>/add-item', methods=["POST"])
 def item_create(wishlist_id):
   item={
@@ -66,6 +94,32 @@ def item_create(wishlist_id):
   return redirect(url_for('wishlists_show', wishlist_id=item['wishlist_id']))
   #wishlist_id=item["wishlist_id"]))
 
+@app.route('/wishlists/<item_id>', methods=['POST'])
+def item_update(item_id):
+  item = items.find_one({'_id':ObjectId(item_id)})
+  updated={
+  'wishlist_id': item['wishlist_id'],
+  'title': request.form.get('title'),
+  'comment':request.form.get('comment'),
+  'link':request.form.get('link'),
+  'img_address':request.form.get('img_address')
+}
+  if updated['img_address'] == '':
+    updated['img_address'] = '/static/images/gifts.png'
+  if updated['link'] == '':
+    updated['link'] = 'https://www.google.com/search?q=no+link&oq=no+link&aqs=chrome..69i57j0i512l9.1218j0j4&sourceid=chrome&ie=UTF-8'
+  items.update_one(
+    {'_id':ObjectId(item_id)},
+    {'$set': updated})
+  return redirect(url_for('wishlists_show', wishlist_id=item['wishlist_id']))
+ 
+
+
+@app.route('/wishlists/<item_id>/delete', methods=['POST'])
+def item_delete(item_id):
+  item = items.find_one({'_id':ObjectId(item_id)})
+  items.delete_one({'_id': ObjectId(item_id)})  
+  return redirect(url_for('wishlists_show', wishlist_id=item['wishlist_id']))
 
 
 # ----------- RUN ----------------------------------------------------- #
